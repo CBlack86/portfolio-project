48049dc87b2bc0b50e076a8fc8696589d89b470f
Cody Black <cdblack86@gmail.com>
2023-06-12 13:01:09 -0400

    Added Router element for page navigation
    Added Browswer Router to create url navigation for
    the app. Also created a Welcome.tsx for my intro
    splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import Projects from "./Projects.tsx";
 import About from "./About.tsx";
 import Contact from "./Contact.tsx";
+import Welcome from "./Welcome.tsx";
+import { Routes, Route, Outlet } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Welcome />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
       
-      <p>
-        This is a test for my main page
-      </p>
-      <About />
-      <Projects />
-      <Contact />
+     
+      
     </>
   )
+}
+function Layout() {
+  return (
+    <div style={{backgroundColor: '#d9d9d9'}}>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {RecoilRoot} from 'recoil';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
 )
