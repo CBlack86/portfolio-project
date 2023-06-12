ffc6b3b2189ab2d0d2ca67eca70da2e54ede0c21
Cody Black <cdblack86@gmail.com>
2023-06-12 11:58:25 -0400

    Changed About.tsx and created new components
    I changed the default About.tsx code to a simple
    placeholer line. I also created teh About.tsx and
    Projects.tsx components and started filling in content.

---
+++ src/About.tsx
@@ -0,0 +1,34 @@
+// Copyright 2023 CODY BLACK
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const About = () => {
+    
+  return (
+      <>
+         <p>
+             Welcome to my page, my name is Cody Black and I am an aspiring full stack software
+             engineer. While receiving my Master's Degree in CyberSecurity I was required to learn
+             python which I quickly picked up. I loved the challenge that coding provided and after
+             graduating I have working on supplementing the skills I have. After looking at he landscape
+             for potential jobs I decided to pivot from Python to leraning other languages so I could
+             become a full stack engineer. I started freeCodeCamp at the beginning of February 2023
+             and have not looked back. I have now complete the first three certifications for this 
+             bootcamp and now having a greater understanding of HTML, CSS, Javascript, and React this
+             site is going to be where I share the projects I am working on. Please enjoy and if you'd
+             like to reach out to me, go to my contacts page; I'm always happy to speak with new people. 
+         </p> 
+      </>
+  )
+}
+export default About;
---
+++ src/Projects.tsx
@@ -0,0 +1,39 @@
+// Copyright 2023 CODY BLACK
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const Projects = () => {
+    
+  return (
+      <>
+          <div>
+              <span>Open Source Projects</span>
+                  <ul>
+                      <li>Group Calclator Project</li>
+                  </ul>
+          </div>
+          <div>
+              <span>freeCodeCamp Projects</span>
+                  <ul>
+                      <li>"25 + 5 Clock"</li>
+                      <li>"Javascript Calculator"</li>
+                      <li>"Drum Machine"</li>
+                      <li>"Markdown Previewer"</li>
+                      <li>"Random Quote Machine"</li>
+                  </ul>
+          </div>
+      </>
+  )
+}
+
+export default Projects;
